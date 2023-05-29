9c5e0898df1e3151c6a01357d61b1fb4f03e11ec
Ferdinand Sangap <fsangap18@gmail.com>
2023-05-29 10:20:17 +0700

    add new table 'archives'

--- src/main/java/id/me/blog/user/User.java
+++ src/main/java/id/me/blog/user/User.java
@@ -17,6 +17,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="authentication_id")
     private long id;
 
     private String username;
--- src/main/resources/db/migration/V0__Init_Table.sql
+++ src/main/resources/db/migration/V0__Init_Table.sql
@@ -1,8 +1,21 @@
 -- authentication --
 CREATE TABLE IF NOT EXISTS authentication(
-    id BIGSERIAL PRIMARY KEY,
-    username text NOT NULL,
-    password text NOT NULL,
-    email text NOT NULL,
-    roles text
+    authentication_id BIGSERIAL                             NOT NULL
+        CONSTRAINT authentication_pk
+            PRIMARY KEY,
+    username          text                                  NOT NULL,
+    password          text                                  NOT NULL,
+    email             text                                  NOT NULL,
+    roles             text
+);
+
+-- archives --
+CREATE TABLE IF NOT EXISTS archives(
+    archive_id BIGSERIAL                                    NOT NULL
+        CONSTRAINT archives_pk
+            PRIMARY KEY ,
+    title      text                                         NOT NULL,
+    body       text                                         NOT NULL,
+    author     text                                         NOT NULL,
+    created_at timestamp with time zone                     NOT NULL
 );
